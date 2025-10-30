7e7b3503c12e6991c5b4862df587d7283ad9b41e
Andreas Lösel <andreas.loesel@outlook.com>
2025-10-30 17:13:54 +0100

    Add listview directory fontsize

--- project-title/2-pt-modify-font-sizes.lua
+++ project-title/2-pt-modify-font-sizes.lua
@@ -20,9 +20,11 @@
 -- vvvvvvvvvvvvvvvvvvvvvvvvvvv-Modify here-vvvvvvvvvvvvvvvvvvvvvvvvvvvvv -
 
 -- List view
-local TITLE_FONT_NOMINAL = 20    -- Default: 20 -- Nominal font size for book titles (and directory names) in list view
-local TITLE_FONT_MAX = 20        -- Default: 26 -- Maximum font size for book titles (and directory names) in list view
-local TITLE_FONT_MIN = 20        -- Default: 20 -- Minimum font size for book titles (and directory names) in list view
+local LIST_DIR_FONT_NOMINAL = 20 -- Default: 20 -- Nominal font size for directories in list view
+local LIST_DIR_FONT_MAX = 26     -- Default: 26 -- Nominal font size for directories in list view
+local TITLE_FONT_NOMINAL = 20    -- Default: 20 -- Nominal font size for book titles in list view
+local TITLE_FONT_MAX = 20        -- Default: 26 -- Maximum font size for book titles in list view
+local TITLE_FONT_MIN = 20        -- Default: 20 -- Minimum font size for book titles in list view
 local AUTHORS_FONT_NOMINAL = 14  -- Default: 14 -- Nominal font size for authors in list view
 local AUTHORS_FONT_MAX = 18      -- Default: 18 -- Maximum font size for authors in list view
 local AUTHORS_FONT_MIN = 10      -- Default: 10 -- Minimum font size for authors in list view
@@ -32,8 +34,8 @@ local TAGS_FONT_MIN = 10         -- Default: 10 -- Minimum font size for tags in
 local TAGS_FONT_OFFSET = 3       -- Default:  3 -- Offset from author font size for tags in list view (e.g. tags are 3 steps smaller than authors)
 
 -- Grid view
-local DIR_FONT_NOMINAL = 18 -- Default: 22 -- Nominal (and maximum) font size for directory names in overlay in grid view
-local DIR_FONT_MIN = 18     -- Default: 18 -- Minimum font size for directory names in overlay in grid view
+local GRID_DIR_FONT_NOMINAL = 18 -- Default: 22 -- Nominal (and maximum) font size for directory names in overlay in grid view
+local GRID_DIR_FONT_MIN = 18     -- Default: 18 -- Minimum font size for directory names in overlay in grid view
 
 -- ^^^^^^^^^^^^^^^^^^^^^^^^^^^-Modify here-^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ -
 
@@ -44,6 +46,8 @@ local function patchCoverBrowser(plugin)
     local ptutil = require("ptutil")
 
     -- All we have to do is overwrite the defaults in ptutil.
+    ptutil.list_defaults.directory_font_nominal = LIST_DIR_FONT_NOMINAL
+    ptutil.list_defaults.directory_font_max = LIST_DIR_FONT_MAX
     ptutil.list_defaults.title_font_nominal = TITLE_FONT_NOMINAL
     ptutil.list_defaults.title_font_max = TITLE_FONT_MAX
     ptutil.list_defaults.title_font_min = TITLE_FONT_MIN
@@ -54,8 +58,8 @@ local function patchCoverBrowser(plugin)
     ptutil.list_defaults.wright_font_max = PROGRESS_FONT_MAX
     ptutil.list_defaults.tags_font_min = TAGS_FONT_MIN
     ptutil.list_defaults.tags_font_offset = TAGS_FONT_OFFSET
-    ptutil.grid_defaults.dir_font_nominal = DIR_FONT_NOMINAL
-    ptutil.grid_defaults.dir_font_min = DIR_FONT_MIN
+    ptutil.grid_defaults.dir_font_nominal = GRID_DIR_FONT_NOMINAL
+    ptutil.grid_defaults.dir_font_min = GRID_DIR_FONT_MIN
 end
 
 userpatch.registerPatchPluginFunc("coverbrowser", patchCoverBrowser)
